de6c88aad2b56859402d99ebed3c77ede1cc08f5
novajhasley <novahasley@gmail.com>
2024-04-18 11:23:50 -0500

    creates basic framework for app including homepage, classes, router, and todos for localization

---
+++ lib/classes/language_constants.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String languageCodeString = 'languageCode';
+
+//languages code
+const String english = 'en';
+const String spanish = 'es';
+
+Future<Locale> setLocale(String languageCode) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString(languageCodeString, languageCode);
+  return _locale(languageCode);
+}
+
+Future<Locale> getLocale() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String languageCode = prefs.getString(languageCodeString) ?? english;
+  return _locale(languageCode);
+}
+
+Locale _locale(String languageCode) {
+  switch (languageCode) {
+    case english:
+      return const Locale(english, '');
+    case spanish:
+      return const Locale(spanish, '');
+    default:
+      return const Locale(english, '');
+  }
+}
